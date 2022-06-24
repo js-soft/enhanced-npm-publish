dc5d336ae766dff3bf381422bc2009be86433f53
jkoenig <julian.koenig@js-soft.com>
2022-06-24 16:54:08 +0200

    feat: add --use-preid-as-tag flag
    this flag uses the 'preid' as the tag
    e.g. '1.0.0-alpha.1' becomes '--tag alpha' in npm publish

--- index.js
+++ index.js
@@ -26,21 +26,37 @@ async function existsPackageInRegistry(pkg) {
   )
 }
 
+const newFlags = ["--if-possible", "--use-preid-as-tag"]
+
 async function run() {
+  if (process.argv.includes("--help") || process.argv.includes("-h")) {
+    console.log(`This command will publish your package to the npm registry. It adds the following flags to the original publish command:
+    --if-possible - If the package is already published, it will not publish it again.
+    --use-preid-as-tag - Will use the 'preid' as the tag. e.g. '1.0.0-alpha.1' -> '--tag alpha'`)
+    process.exit(0)
+  }
+
   const pkg = getPackageInfo()
   const isPublished = await existsPackageInRegistry(pkg)
 
   if (process.argv.includes("--if-possible") && isPublished) {
     console.log(`${pkg.name} with version ${pkg.version} is already published.`)
     process.exit(0)
-  } else {
-    let args = [...process.argv]
-    args.splice(0, 2)
-    args = args.filter((arg) => arg !== "--if-possible")
-
-    const child = spawn("npm", ["publish", ...args], { stdio: "inherit" })
-    child.on("exit", (code) => process.exit(code))
   }
+
+  let args = [...process.argv]
+  args.splice(0, 2)
+  args = args.filter((arg) => !newFlags.includes(arg))
+
+  const semverWithPreidRegex = /^\d+\.\d+\.\d+(?:-([a-z]+)(?:\.\d+)+)?$/
+  const semverMatches = semverWithPreidRegex.exec(pkg.version)
+  if (process.argv.includes("--use-preid-as-tag") && semverMatches && semverMatches[1]) {
+    const preid = semverMatches[1]
+    args.push("--tag", preid)
+  }
+
+  const child = spawn("npm", ["publish", ...args], { stdio: "inherit" })
+  child.on("exit", (code) => process.exit(code))
 }
 
 run()
